563f0fa2bd4c7609a6922e02eb7b9e9fcb13f45a
John Turner <johnturner@me.com>
2022-02-06 21:14:58 +0000

    chore: remove smui

--- src/routes/links.svelte
+++ src/routes/links.svelte
@@ -3,30 +3,22 @@
 </svelte:head>
 
 <script lang="ts">
-  import Paper, { Title, Content } from '@smui/paper';
-  import DataTable, { Head, Body, Row, Cell } from "@smui/data-table";
-
   export let links;
 </script>
 
-<Paper>
-  <Title>Links</Title>
-  <Content>
-    <DataTable style="max-width: 100%">
-      <Head>
-        <Row>
-          <Cell>Label</Cell>
-          <Cell>Destination</Cell>
-        </Row>
-      </Head>
-      <Body>
-        {#each links as link}
-          <Row>
-            <Cell>{link.label}</Cell>
-            <Cell>{link.destination}</Cell>
-          </Row>
-        {/each}
-      </Body>
-    </DataTable>
-  </Content>
-</Paper>
+<table>
+  <thead>
+    <tr>
+      <th>Label</th>
+      <th>Destination</th>
+    </tr>
+  </thead>
+  <tbody>
+    {#each links as link}
+      <tr>
+        <td>{link.label}</td>
+        <td>{link.destination}</td>
+      </tr> 
+    {/each}
+  </tbody>
+</table>

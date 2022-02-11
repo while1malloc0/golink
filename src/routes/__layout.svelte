054a50ec0e28c8bc5d99c4ae83a1da5f79188624
John Turner <johnturner@me.com>
2022-02-11 03:35:36 +0000

    chore: add Tailwind deps

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,14 @@
 <script>
+  import "../app.css";
 </script>
 
+<header>
+  <div class="container px-2 py-10">
+    <h1 class="text-3xl">Go/Link</h1>
+  </div>
+</header>
 <main>
-  <slot/>
+  <div class="container px-2">
+    <slot/>
+  </div>
 </main>
---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{js,ts,svelte,html}"],
+  theme: {
+    extend: {
+      colors: {
+        "golink-green": "5A7B6A",
+        "golink-cream": "EDD9B2",
+        "golink-orange": "CE7C5B",
+        "golink-red": "B8402F",
+        "golink-beige": "CEA27F",
+      },
+    },
+  },
+  plugins: [],
+}

506d3cd3e4919fa403bf076d5861d27d2441a051
John Turner <johnturner@me.com>
2022-02-12 01:25:17 +0000

    fix: don't parse cookie if it doesn't exist

--- src/hooks.ts
+++ src/hooks.ts
@@ -4,11 +4,14 @@ import type { Handle } from "@sveltejs/kit";
 import { sequence } from "@sveltejs/kit/hooks";
 
 const injectCurrentUser: Handle = async ({ event, resolve }) => {
-  const { session_id: sessionId } = parse(event.request.headers.get("cookie"));
-  if (sessionId) {
-    const session = await getSession(sessionId);
-    if (session) {
-      event.locals.user = session.user;
+  const cookieHeader = event.request.headers.get("cookie");
+  if (cookieHeader) {
+    const { session_id: sessionId } = parse(cookieHeader);
+    if (sessionId) {
+      const session = await getSession(sessionId);
+      if (session) {
+        event.locals.user = session.user;
+      }
     }
   }
   return resolve(event);

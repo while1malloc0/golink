03b539c85a4068f7676c8bec43b5f284fa5ec1dd
John Turner <johnturner@me.com>
2022-02-18 03:26:01 +0000

    refactor(style): make links table a little nicer

--- src/routes/links/index.svelte
+++ src/routes/links/index.svelte
@@ -6,19 +6,21 @@
   export let links;
 </script>
 
-<table>
-  <thead>
-    <tr>
-      <th>Label</th>
-      <th>Destination</th>
-    </tr>
-  </thead>
-  <tbody>
-    {#each links as link}
+<div class="bg-white p-3 rounded-md shadow-lg">
+  <table class="m-1 w-full">
+    <thead class="tracking-wide text-md font-semibold text-left bg-gray-200">
       <tr>
-        <td>{link.label}</td>
-        <td>{link.destination}</td>
-      </tr> 
-    {/each}
-  </tbody>
-</table>
+        <th>Label</th>
+        <th>Destination</th>
+      </tr>
+    </thead>
+    <tbody class="text-left tracking-wide font-light text-sm">
+      {#each links as link}
+        <tr class="hover:bg-gray-50">
+          <td>{link.label}</td>
+          <td>{link.destination}</td>
+        </tr> 
+      {/each}
+    </tbody>
+  </table>
+</div>

ff90d70a43dbd8e1aab3f0d251b092b08df3b708
John Turner <johnturner@me.com>
2022-02-19 02:02:09 +0000

    feat: add editing UI for links

--- src/routes/links/index.svelte
+++ src/routes/links/index.svelte
@@ -4,20 +4,45 @@
 
 <script lang="ts">
   import Card from "$lib/components/Card.svelte";
-  import Table from "$lib/components/Table.svelte";
   import Pencil from "$lib/icons/Pencil.svelte";
+  import Check from "$lib/icons/Check.svelte";
   export let links;
 
   const tableData = links.map((link) => {
-    return {label: link.label, destination: link.destination, edit: Pencil}
+    return {...link, edit: false}
   });
-  const columns = [
-    {title: "Label", field: "label"},
-    {title: "Destination", field: "destination"},
-    {title: "", field: "edit"},
-  ]
+
+  const toggleEditState = (link) => {
+    const idx = tableData.findIndex((l) => l.label === link.label);
+    if (idx > -1) {
+      tableData[idx].edit = !tableData[idx].edit;
+    }
+  }
 </script>
 
 <Card>
-  <Table columns={columns} items={tableData}/>
+  <table class="m-1 w-full">
+    <thead class="tracking-wide text-md font-semibold text-left bg-gray-200">
+      <tr>
+        <th>Label</th>
+        <th>Destination</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody class="text-left tracking-wide font-light text-sm">
+        {#each tableData as link}
+          <tr class="hover:bg-gray-50">
+            {#if link.edit}
+              <td><input class="tracking-wide m-0 font-light text-sm text-left rounded-md ring-1 ring-golink-green px-1 w-min" value={link.label}/></td>
+              <td><input class="tracking-wide m-0 font-light text-sm text-left rounded-md ring-1 ring-golink-green px-1 w-full" value={link.destination}/></td>
+              <td on:click={() => toggleEditState(link)}><Check/></td>
+            {:else}
+              <td>{link.label}</td>
+              <td>{link.destination}</td>
+              <td on:click={() => toggleEditState(link)}><Pencil/></td>
+            {/if}
+          </tr> 
+        {/each}
+    </tbody>
+  </table>
 </Card>
